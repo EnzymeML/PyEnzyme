5bfb968ea817e92a4df11e528b2a8679a0bff7cf
JR-1991 <30547301+JR-1991@users.noreply.github.com>
2020-10-22 13:53:51 +0200

    Add files via upload

--- pyenzyme/Examples/ThinLayers/TL_Copasi.py
+++ pyenzyme/Examples/ThinLayers/TL_Copasi.py
@@ -8,6 +8,7 @@ from pyenzyme.enzymeml.tools import EnzymeMLReader, EnzymeMLWriter
 import os
 
 import COPASI
+from builtins import enumerate
 
 class ThinLayerCopasi(object):
     
@@ -23,6 +24,8 @@ class ThinLayerCopasi(object):
             String reaction: Reaction ID to extract data from
             String reactants: Single or multiple IDs for desired reactant(s)
         '''
+        if type(reactants) == str:
+            reactants = [reactants]
         
         fname = str(os.path.basename(path)).split(".omex")[0]
         
@@ -39,11 +42,6 @@ class ThinLayerCopasi(object):
         
         # Get reaction and replicates
         reaction = enzmldoc.getReaction(reaction)
-                
-        repls = reaction.exportReplicates(reactants)
-        repls.columns = [ column.split('/')[1] for column in repls.columns ]
-        repls.index.name = "time"
-        repls.to_csv( outdir + '/data.tsv', sep='\t')
         
         # initialize COPASI data model
         dm = COPASI.CRootContainer.addDatamodel()
@@ -53,10 +51,8 @@ class ThinLayerCopasi(object):
         ########### COPASI ###########
         
         # Get Reactant specific ConcentrationReference
-        all_elems = reaction.getEducts() + reaction.getProducts() + reaction.getModifiers()
-        reaction_species = [ tup[0] for tup in all_elems ]
         col_cn_map = {metab.sbml_id : metab.getConcentrationReference().getCN()
-              for metab in dm.getModel().getMetabolites() if metab.sbml_id in repls.columns}
+              for metab in dm.getModel().getMetabolites() if metab.sbml_id in reactants}
         
         # Describe .tsv file and call COPASI bindings
         task = dm.getTask('Parameter Estimation')
@@ -65,37 +61,59 @@ class ThinLayerCopasi(object):
         problem.setCalculateStatistics(False)
         exp_set = problem.getExperimentSet()
         
-        exp = COPASI.CExperiment(dm)
-        exp = exp_set.addExperiment(exp)
-        info = COPASI.CExperimentFileInfo(exp_set)
-        info.setFileName( outdir + "/data.tsv")
-        exp.setObjectName(reaction.getName())
-        exp.setFirstRow(1)
-        exp.setLastRow(repls.shape[0]+1)
-        exp.setHeaderRow(1)
-        exp.setFileName( outdir + "/data.tsv")
-        exp.setExperimentType(COPASI.CTaskEnum.Task_timeCourse)
-        exp.setSeparator('\t')
-        exp.setNumColumns(repls.shape[1]+1)
         
-        # Mapping from .tsv file to COPASI bindings
-        obj_map = exp.getObjectMap()
-        obj_map.setNumCols(repls.shape[1]+1)
         
-        for i, col in enumerate(["time"] + list(repls.columns)):
-            
-            if col is "time":
-                role = COPASI.CExperiment.time
-                obj_map.setRole(i, role)
+        for reactant in reactants:
+            # include each replicate
+            for i, repl in enumerate( reaction.getEduct(reactant)[3] ):
                 
-            elif col in col_cn_map.keys():
-                role = COPASI.CExperiment.dependent
-                obj_map.setRole(i, role)
-                obj_map.setObjectCN(i, col_cn_map[col])
+                data = repl.getData()
+                data.name = data.name.split('/')[1]
                 
-            else:
-                role = COPASI.CExperiment.ignore
-                obj_map.setRole(i, role)
+                conc_val, conc_unit =  enzmldoc.getConcDict()[repl.getInitConc()]
+                
+                metab = [ metab for metab in dm.getModel().getMetabolites() if metab.sbml_id == reactant][0]
+                cn = metab.getInitialConcentrationReference().getCN()
+                
+                data.to_csv( outdir +'/experiment_%s_%i.tsv' % ( reactant, i ), sep='\t', header=True)
+                
+                exp = COPASI.CExperiment(dm)
+                exp = exp_set.addExperiment(exp)
+                info = COPASI.CExperimentFileInfo(exp_set)
+                info.setFileName( outdir + '/experiment_%s_%i.tsv' % ( reactant, i ))
+                exp.setObjectName(reaction.getName())
+                exp.setFirstRow(1)
+                exp.setLastRow(data.shape[0]+1)
+                exp.setHeaderRow(1)
+                exp.setFileName( outdir + '/experiment_%s_%i.tsv' % ( reactant, i ))
+                exp.setExperimentType(COPASI.CTaskEnum.Task_timeCourse)
+                exp.setSeparator('\t')
+                exp.setNumColumns(2)
+                
+                # add the initial concentration as fit item
+                item = problem.addFitItem(cn)
+                item.setStartValue(conc_val)  # the current initial concentration 
+                item.setLowerBound(COPASI.CCommonName(str(conc_val * 0.01)))
+                item.setUpperBound(COPASI.CCommonName(str(conc_val * 100)))
+                item.addExperiment(exp.getKey()) # the key of the current experiment
+
+                # Mapping from .tsv file to COPASI bindings
+                obj_map = exp.getObjectMap()
+                obj_map.setNumCols(2)
+                
+                for i, col in enumerate(["time"] + [data.name]):
+                    if col is "time":
+                        role = COPASI.CExperiment.time
+                        obj_map.setRole(i, role)
+                        
+                    elif col in col_cn_map.keys():
+                        role = COPASI.CExperiment.dependent
+                        obj_map.setRole(i, role)
+                        obj_map.setObjectCN(i, col_cn_map[col])
+                        
+                    else:
+                        role = COPASI.CExperiment.ignore
+                        obj_map.setRole(i, role)
                 
         # Finally save the model and add plot/progress
         dm.saveModel( outdir + "/" + fname + ".cps", True)
@@ -112,4 +130,4 @@ class ThinLayerCopasi(object):
         
 if __name__ == '__main__':
     
-    ThinLayerCopasi().importEnzymeML('r0', 's0', "../../Resources/Examples/ThinLayers/COPASI/3IZNOK_TEST/3IZNOK_TEST.omex")
+    ThinLayerCopasi().importEnzymeML('r0', 's1', "../../Resources/Examples/ThinLayers/COPASI/3IZNOK_TEST/3IZNOK_TEST.omex")
