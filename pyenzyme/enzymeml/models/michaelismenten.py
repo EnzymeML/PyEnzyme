d543203d8806a5992d62ebcaaafc2e07a134b331
JR-1991 <range.jan@web.de>
2021-03-09 15:06:36 +0100

    - Added unit checks for consistency

--- pyenzyme/enzymeml/models/michaelismenten.py
+++ pyenzyme/enzymeml/models/michaelismenten.py
@@ -5,12 +5,17 @@ Created on 10.06.2020
 '''
 from pyenzyme.enzymeml.core.functionalities import TypeChecker
 from pyenzyme.enzymeml.models import KineticModel
+from pyenzyme.enzymeml.tools.unitcreator import UnitCreator
 from libsbml._libsbml import parseL3Formula
 import json
 
-def MichaelisMenten(vmax_val, vmax_unit, km_val, km_unit ,substrate_id):
+def MichaelisMenten(vmax_val, vmax_unit, km_val, km_unit ,substrate_id, enzmldoc):
     
     equation = f"vmax_{substrate_id} * ( {substrate_id} / ({substrate_id} + km_{substrate_id}) )"
+    
+    vmax_unit = UnitCreator().getUnit(vmax_unit, enzmldoc)
+    km_unit = UnitCreator().getUnit(km_unit, enzmldoc)
+    
     parameters = {
         
         f"vmax_{substrate_id}": (vmax_val, vmax_unit),
