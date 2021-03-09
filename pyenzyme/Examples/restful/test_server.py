39fd80b3aa241bf0366198508b2ec840886b0aab
JR-1991 <range.jan@web.de>
2021-03-09 15:08:02 +0100

    - Added COPASI functionality

--- pyenzyme/Examples/restful/test_server.py
+++ pyenzyme/Examples/restful/test_server.py
@@ -1,14 +1,15 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 
-from pyenzyme.restful import Create, Read
+from pyenzyme.restful import Create, Read, restfulCOPASI
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Create, '/create')
 api.add_resource(Read,'/read' )
+api.add_resource(restfulCOPASI, '/copasi')
 
 if __name__ == "__main__":
     
-    app.run(debug=False)
+    app.run(debug=True)
