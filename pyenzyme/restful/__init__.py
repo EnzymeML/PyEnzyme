20f035f428f82ff2be97f8e1ddc9980905b6f004
JR-1991 <range.jan@web.de>
2021-04-01 00:32:02 +0200

    - Extended REST API with spreadsheet converter function

--- pyenzyme/restful/__init__.py
+++ pyenzyme/restful/__init__.py
@@ -1,11 +1,12 @@
 # @Author: Jan Range
 # @Date:   2021-03-18 22:33:21
 # @Last Modified by:   Jan Range
-# @Last Modified time: 2021-03-26 18:35:32
+# @Last Modified time: 2021-03-30 14:03:46
 
 from pyenzyme.restful.create import Create
 from pyenzyme.restful.read import Read
 from pyenzyme.restful.copasi import restfulCOPASI
 from pyenzyme.restful.model import parameterEstimation
+from pyenzyme.restful.template import convertTemplate
 from pyenzyme.restful.server import RestfulServer
 from pyenzyme.restful.create_schema import EnzymeMLSchema
