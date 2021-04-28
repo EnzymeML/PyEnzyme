46b8b7331dd5d01e6048ab3e9543ea500a9ac559
JR-1991 <range.jan@web.de>
2021-04-28 10:34:33 +0200

    ENZML-70 Added exportData functionality and Swagger doc

--- pyenzyme/Examples/restful/run_server.py
+++ pyenzyme/Examples/restful/run_server.py
@@ -1,7 +1,7 @@
 # @Author: Jan Range
 # @Date:   2021-03-18 22:33:21
 # @Last Modified by:   Jan Range
-# @Last Modified time: 2021-04-20 09:13:10
+# @Last Modified time: 2021-04-28 10:33:35
 from flask import Flask, request
 from flask_restful import Resource, Api
 from apispec import APISpec
@@ -9,7 +9,7 @@ from apispec.ext.marshmallow import MarshmallowPlugin
 from flask_apispec import FlaskApiSpec
 from flask_cors import CORS
 
-from pyenzyme.restful import Create, Read, restfulCOPASI, parameterEstimation, convertTemplate
+from pyenzyme.restful import Create, Read, restfulCOPASI, parameterEstimation, convertTemplate, exportData
 
 app = Flask(__name__)
 api = Api(app)
@@ -23,11 +23,13 @@ api.add_resource(Read,'/read' )
 api.add_resource(restfulCOPASI, '/copasi')
 api.add_resource(parameterEstimation, '/model')
 api.add_resource(convertTemplate, '/template/convert')
+api.add_resource(exportData, '/exportdata')
 
 docs.register(Create, endpoint='create')
 docs.register(Read, endpoint='read')
 docs.register(parameterEstimation, endpoint='parameterestimation')
 docs.register(convertTemplate, endpoint='converttemplate')
+docs.register(exportData, endpoint='exportdata')
 
 if __name__ == "__main__":
         
--- pyenzyme/restful/__init__.py
+++ pyenzyme/restful/__init__.py
@@ -1,12 +1,13 @@
 # @Author: Jan Range
 # @Date:   2021-03-18 22:33:21
 # @Last Modified by:   Jan Range
-# @Last Modified time: 2021-03-30 14:03:46
+# @Last Modified time: 2021-04-27 11:50:45
 
 from pyenzyme.restful.create import Create
 from pyenzyme.restful.read import Read
 from pyenzyme.restful.copasi import restfulCOPASI
 from pyenzyme.restful.model import parameterEstimation
 from pyenzyme.restful.template import convertTemplate
+from pyenzyme.restful.exportData import exportData
 from pyenzyme.restful.server import RestfulServer
 from pyenzyme.restful.create_schema import EnzymeMLSchema
