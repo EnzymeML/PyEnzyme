e346e97acfed7d166b1980a172c7c59df13a7555
JR-1991 <range.jan@web.de>
2021-04-29 21:27:42 +0200

    ENZML-70 Unique directory for temporary files

--- pyenzyme/restful/exportData.py
+++ pyenzyme/restful/exportData.py
@@ -1,7 +1,7 @@
 # @Author: Jan Range
 # @Date:   2021-03-18 22:33:21
 # @Last Modified by:   Jan Range
-# @Last Modified time: 2021-04-28 10:28:50
+# @Last Modified time: 2021-04-29 19:39:59
 
 from flask import Flask, request, send_file, jsonify, Response
 from flask_restful import Resource, Api
@@ -51,7 +51,7 @@ class exportData(MethodResource):
             file = file.read()
             
             # Write to temp file
-            dirpath = os.path.join( os.path.dirname( os.path.realpath(__file__)), "read_temp" )
+            dirpath = os.path.join( os.path.dirname( os.path.realpath(__file__)), "exportData_temp" )
             os.makedirs(dirpath, exist_ok=True)
             
             tmp = os.path.join( dirpath, next(tempfile._get_candidate_names()) )
