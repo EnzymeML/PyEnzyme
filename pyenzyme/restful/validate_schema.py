4adc7b2449b2dfa6fdf6057bedb7cbbc36917bc1
JR-1991 <range.jan@web.de>
2021-05-11 18:39:01 +0200

    ENZML-84 Removed unused imports and extended docstring

--- pyenzyme/restful/validate_schema.py
+++ pyenzyme/restful/validate_schema.py
@@ -1,15 +1,17 @@
 # @Author: Jan Range
 # @Date:   2021-03-19 15:03:19
 # @Last Modified by:   Jan Range
-# @Last Modified time: 2021-05-06 20:18:09
+# @Last Modified time: 2021-05-11 18:38:23
 
-from marshmallow import fields, Schema, ValidationError
+from marshmallow import fields, Schema
 
 class ValidateSchema(Schema):
-    
-    ############# JSON #################
+    """Schema describing the Form-Data request to the calidation endpoint
+    """
     
     class JSONSchema(Schema):
+        """Schema describing the JSON part 
+        """
         link = fields.Str()
         custom = fields.Str()
     
