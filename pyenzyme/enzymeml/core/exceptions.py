cea8604ffea89e5204eac6ad3724e555abcf3246
JR-1991 <30547301+JR-1991@users.noreply.github.com>
2021-11-30 22:18:42 +0100

    Added exceptions to handle initializer errors

--- pyenzyme/enzymeml/core/exceptions.py
+++ pyenzyme/enzymeml/core/exceptions.py
@@ -10,6 +10,8 @@ Modified By: Jan Range (<jan.range@simtech.uni-stuttgart.de>)
 Copyright (c) 2021 Institute of Biochemistry and Technical Biochemistry Stuttgart
 '''
 
+from typing import Union
+
 
 class ValidationError(Exception):
     """Raised when a Dataverse validation has failed"""
@@ -81,3 +83,27 @@ class UnitKindError(Exception):
 
 class MissingUnitError(Exception):
     """Raised when a unit is missing """
+
+
+class ChEBIIdentifierError(Exception):
+    """Raised when the CHEBI ID is incorrect."""
+
+    def __init__(self, chebi_id: Union[str, int]) -> None:
+        self.chebi_id = chebi_id
+
+    def __str__(self) -> str:
+        return (
+            f"ChEBI ID {self.chebi_id} is invalid. Please provide a valid ChEBI ID."
+        )
+
+
+class UniProtIdentifierError(Exception):
+    """Raised when the UniProt ID is incorrect."""
+
+    def __init__(self, uniprotid: Union[str, int]) -> None:
+        self.uniprotid = uniprotid
+
+    def __str__(self) -> str:
+        return (
+            f"UniProt ID {self.uniprotid} is invalid. Please provide a valid UniProt ID."
+        )
