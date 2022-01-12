69a3b9821bab93ae29e61566c594d6d1481bd90f
JR-1991 <30547301+JR-1991@users.noreply.github.com>
2022-01-12 19:31:18 +0100

    Added model generator

--- pyenzyme/enzymeml/models/kineticmodel.py
+++ pyenzyme/enzymeml/models/kineticmodel.py
@@ -10,6 +10,8 @@ Modified By: Jan Range (<jan.range@simtech.uni-stuttgart.de>)
 Copyright (c) 2021 Institute of Biochemistry and Technical Biochemistry Stuttgart
 '''
 
+import ast
+
 from typing import TYPE_CHECKING, Optional
 from pydantic import Field
 from dataclasses import dataclass
@@ -38,14 +40,24 @@ class KineticParameter(EnzymeMLBase):
         description="Name of the estimated parameter.",
     )
 
-    value: float = Field(
-        ...,
+    value: Optional[float] = Field(
+        None,
         description="Numerical value of the estimated parameter.",
     )
 
-    unit: str = Field(
-        ...,
+    unit: Optional[str] = Field(
+        None,
         description="Unit of the estimated parameter.",
+    )
+
+    stdev: Optional[float] = Field(
+        None,
+        description="Standard deviation of the estimated parameter."
+    )
+
+    initial_value: Optional[float] = Field(
+        None,
+        description="Initial value that was used for the parameter estimation."
     )
 
     ontology: Optional[SBOTerm] = Field(
@@ -79,8 +91,27 @@ class KineticModel(EnzymeMLBase):
         None,
         description="Type of the estimated parameter.",
     )
-    # ! Utilities
 
+    # ! Initializers
+    @staticmethod
+    def createGenerator(name: str, equation: str, **parameters):
+        """Creates an abstract model generator to generated specific models.
+
+        Args:
+            name (str): Name of the model.
+            equation (str): Equation
+
+        Returns:
+            [type]: [description]
+        """
+
+        return ModelFactory(
+            name=name,
+            equation=equation,
+            **parameters
+        )
+
+    # ! Utilities
     def addToReaction(self, reaction: Reaction) -> None:
         '''
         Adds kinetic law to SBML reaction.
@@ -113,6 +144,28 @@ class KineticModel(EnzymeMLBase):
         return self.name
 
     # ! Getters
+    def getParameter(self, name: str) -> KineticParameter:
+        """Returns a parameter of choice from the model.
+
+        Args:
+            name (str): Name of the parameter.
+
+        Raises:
+            KeyError: If the parameter does not exist.
+
+        Returns:
+            KineticParameter: The desired parameter.
+        """
+
+        try:
+            return next(filter(
+                lambda parm: parm.name == name, self.parameters
+            ))
+        except StopIteration:
+            raise KeyError(
+                f"Parameter {name} not found in the model."
+            )
+
     @deprecated_getter("equation")
     def getEquation(self):
         return self.equation
@@ -124,3 +177,73 @@ class KineticModel(EnzymeMLBase):
     @deprecated_getter("name")
     def getName(self):
         return self.name
+
+
+class ModelFactory:
+
+    equation: str
+    parameters: list[str]
+    name: str
+
+    def __init__(self, name: str, equation: str, **parameters) -> None:
+
+        # Parse the eqation and get all names and variables
+        self.variables = self.parse_equation(equation)
+
+        # Initialize the model
+        self.model = KineticModel(
+            name=name,
+            equation=equation,
+            ontology=None
+        )
+
+        for name, options in parameters.items():
+
+            # Remove parameter from variables
+            self.variables.remove(name)
+
+            # Get all teh options for the parameter
+            init_value = options.get("init_value")
+            value = options.get("value")
+            unit = options.get("unit")
+            ontology = options.get("ontology")
+            stdev = options.get("stdev")
+
+            parameter = KineticParameter(
+                name=name,
+                value=value,
+                unit=unit,
+                initial_value=init_value,
+                stdev=stdev,
+                ontology=ontology
+            )
+
+            self.model.parameters.append(parameter)
+
+    def __call__(self, **variables) -> KineticModel:
+        """Returns a KineticModel that is suited for the given parameters.
+        """
+
+        # Copy the internal object and modify it to the needs
+        model = self.model.copy()
+
+        # Replace everything
+        for stock_variable in self.variables:
+
+            try:
+                identifier: str = variables[stock_variable]
+                model.equation = model.equation.replace(
+                    stock_variable, identifier)
+            except KeyError:
+                raise KeyError(
+                    f"Variable {stock_variable} has not been given. Please make sure to cover all variables: [{repr(self.variables)}]"
+                )
+
+        return model
+
+    @staticmethod
+    def parse_equation(equation: str):
+        return [
+            node.id for node in ast.walk(ast.parse(equation))
+            if isinstance(node, ast.Name)
+        ]
