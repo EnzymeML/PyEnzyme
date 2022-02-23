a07c94065e44d39b1867384850f5413f04d1997c
JM Rohwer <j.m.rohwer@gmail.com>
2022-02-23 09:39:36 +0200

    Fix typo

--- pyenzyme/thinlayers/__init__.py
+++ pyenzyme/thinlayers/__init__.py
@@ -7,7 +7,7 @@ try:
     from .TL_Pysces import ThinLayerPysces
 except ModuleNotFoundError as e:
     print(
-        "%s - ThinLayerCopasi now returns 'None', please provide the dependencies to use this module." % str(e)
+        "%s - ThinLayerPysces now returns 'None', please provide the dependencies to use this module." % str(e)
     )
     ThinLayerPysces = None
 
