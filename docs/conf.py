27750e743286c431b32de4e8715e86f62c12b65a
JR-1991 <30547301+JR-1991@users.noreply.github.com>
2022-03-20 10:24:14 +0100

    Fix failed doc

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,6 @@ extensions = [
     "IPython.sphinxext.ipython_console_highlighting",
     "IPython.sphinxext.ipython_directive",
     "sphinx.ext.autosectionlabel",
-    "furo",
 ]
 
 autodoc_pydantic_model_show_json = True
