d3a9fcf144c0075d48458ffc63dd5d77b267ce68
JR-1991 <30547301+JR-1991@users.noreply.github.com>
2022-03-25 11:47:58 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Jan Range"
 author = "Jan Range, Dr. Frank Bergmann, Prof. Dr. Johann Rohwer"
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.3"
+release = "1.2.3"
 
 
 # -- General configuration ---------------------------------------------------
