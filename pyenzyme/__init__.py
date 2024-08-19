978e100ef734f5a6d06361aeba2268d736f0c36c
Jan Range <range.jan@web.de>
2024-08-19 21:25:39 +0200

    attach readers and writers to model class

--- pyenzyme/__init__.py
+++ pyenzyme/__init__.py
@@ -1,28 +1,32 @@
 from __future__ import annotations
 
 import json
-from rich import print
 from pathlib import Path
 
+from rich import print
+
 from .model import *  # noqa: F403
-from .sbml import to_sbml  # noqa: F401
+from .sbml import to_sbml, read_sbml  # noqa: F401
 from .tabular import to_pandas, read_csv, read_excel  # noqa: F401
 
 __all__ = [
-    "load_enzymeml",
+    "read_enzymeml",
     "write_enzymeml",
     "to_sbml",
     "to_pandas",
 ]
 
 
-def load_enzymeml(path: str) -> EnzymeMLDocument:  # noqa: F405
+def read_enzymeml(cls, path: str) -> EnzymeMLDocument:  # noqa: F405
     with open(path) as f:
-        return EnzymeMLDocument.model_validate_json(f.read())  # noqa: F405
+        return cls.model_validate_json(f.read())  # noqa: F405
 
 
-def write_enzymeml(doc: EnzymeMLDocument, path: Path | str | None = None):  # noqa: F405
-    data = json.loads(doc.model_dump_json(exclude_none=True, by_alias=True))
+def write_enzymeml(
+    self: EnzymeMLDocument,  # noqa: F405
+    path: Path | str | None = None,  # noqa: F405
+):  # noqa: F405
+    data = json.loads(self.model_dump_json(exclude_none=True, by_alias=True))
     data = json.dumps(sort_by_ld(data), indent=2)
 
     if path is None:
@@ -37,6 +41,13 @@ def write_enzymeml(doc: EnzymeMLDocument, path: Path | str | None = None):  # no
         f.write(data)
 
     print(f"\n  EnzymeML document written to [green][bold]{path}[/bold][/green]\n")
+
+
+# Add these methods to the EnzymeMLDocument class
+EnzymeMLDocument.read = classmethod(read_enzymeml)  # noqa: F405
+EnzymeMLDocument.write = write_enzymeml  # noqa: F405
+EnzymeMLDocument.from_sbml = classmethod(read_sbml)  # noqa: F405
+EnzymeMLDocument.to_sbml = to_sbml  # noqa: F405
 
 
 def sort_by_ld(d: dict) -> dict:
