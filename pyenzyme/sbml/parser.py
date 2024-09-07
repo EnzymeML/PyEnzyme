7a381d8b1518b7a7ea51a9eecdb1eb53f1caa8dc
Jan Range <range.jan@web.de>
2024-09-07 19:46:32 +0200

    adapt parser and serializer to annotations

--- pyenzyme/sbml/parser.py
+++ pyenzyme/sbml/parser.py
@@ -1,23 +1,20 @@
 from __future__ import annotations
 
 import math
-import xml.etree.ElementTree as ET
+import re
 from pathlib import Path
 from typing import IO
 
 import libsbml as sbml  # type: ignore
-import rich
+import pandas as pd
 from loguru import logger  # type: ignore
 
 import pyenzyme as pe
-from pyenzyme import tools
+from pyenzyme import xmlutils
 from pyenzyme.logging import add_logger
-from pyenzyme.units import M, s
 from . import read_sbml_omex
 from .ldutils import parse_sbml_rdf_annotation
-
-MAPPINGS = tools.read_static_file("pyenzyme.sbml", "mappings.toml")
-ENZYMEML_NS = "https://www.enzymeml.org/v2"
+from .versions.handler import VersionHandler, SupportedVersions
 
 
 def read_sbml(cls, path: Path | str):
@@ -29,20 +26,27 @@ def read_sbml(cls, path: Path | str):
         path (Path | str): The path to the OMEX archive containing the SBML file.
 
     Returns:
-        An initialized EnzymeML document with extracted units, species, vessels, equations, parameters, reactions, and measurements.
+        An initialized EnzymeML document with extracted units, species, vessels,
+        equations, parameters, reactions, and measurements.
     """
     add_logger(name="SBML")
 
     # Create globals to use in the functions
     global units
     global path_prefix
-    global tsv_path
+    global version
+    global enzmldoc
 
     # Read the OMEX archive and extract the SBML and TSV paths
-    sbml_path, tsv_path = read_sbml_omex(path)
+    sbml_handler, data = read_sbml_omex(path)
+
+    # Find out which version of the SBML file we are dealing with
+    namespaces = xmlutils.extract_namespaces(sbml_handler.read())
+    version = VersionHandler.from_uri(namespaces)
+    sbml_handler.seek(0)
 
     # Read the SBML file and init an EnzymeML document
-    model = _init_and_read_sbml(open(sbml_path))
+    model = _init_and_read_sbml(sbml_handler)
     enzmldoc = cls(name=model.getName())
 
     # Extract units to map these to the EnzymeML entities
@@ -76,14 +80,28 @@ def read_sbml(cls, path: Path | str):
         _parse_reaction(reaction) for reaction in model.getListOfReactions()
     ]
 
-    enzmldoc.measurements = _parse_measurements(model)
+    enzmldoc.measurements = _parse_measurements(
+        model=model,
+        list_of_reactions=model.getListOfReactions(),
+        meas_data=data,
+    )
 
     return enzmldoc
 
 
 def _init_and_read_sbml(handler: IO):
-    """Initialize the SBML reader and read the SBML document."""
+    """
+    Initialize the SBML reader and read the SBML document.
 
+    Args:
+        handler (IO): The file handler for the SBML document.
+
+    Returns:
+        The SBML model.
+
+    Raises:
+        ValueError: If the SBML document or model cannot be read.
+    """
     reader = sbml.SBMLReader()
     sbmldoc = reader.readSBMLFromString(handler.read())
     if not sbmldoc:
@@ -100,7 +118,15 @@ def _init_and_read_sbml(handler: IO):
 
 
 def _parse_unit(unit: sbml.UnitDefinition):
-    """Parse a unit definition from an SBML model into an EnzymeML unit definition."""
+    """
+    Parse a unit definition from an SBML model into an EnzymeML unit definition.
+
+    Args:
+        unit (sbml.UnitDefinition): The SBML unit definition.
+
+    Returns:
+        An EnzymeML unit definition.
+    """
     enzml_unit = pe.UnitDefinition(
         id=unit.getId(),
         name=unit.getName(),
@@ -126,7 +152,15 @@ def _parse_unit(unit: sbml.UnitDefinition):
 
 
 def _parse_species(species: sbml.Species):
-    """Parse a species from an SBML model into EnzymeML small molecule or protein."""
+    """
+    Parse a species from an SBML model into EnzymeML small molecule or protein.
+
+    Args:
+        species (sbml.Species): The SBML species.
+
+    Returns:
+        An EnzymeML small molecule, protein, or complex.
+    """
     sbo_term = species.getSBOTermID()
 
     if sbo_term == "SBO:0000247":
@@ -142,9 +176,18 @@ def _parse_species(species: sbml.Species):
 
 
 def _parse_small_molecule(species: sbml.Species):
-    """Parse a species from an SBML model into an EnzymeML small molecule."""
+    """
+    Parse a species from an SBML model into an EnzymeML small molecule.
 
-    annots = _parse_enzymeml_annotations("smallMolecule", species.getAnnotationString())
+    Args:
+        species (sbml.Species): The SBML species.
+
+    Returns:
+        An EnzymeML small molecule.
+    """
+    parsed = version.parse_annotation(annotation=species.getAnnotationString())
+    annots = version.extract(parsed, "small_molecule")
+
     small_molecule = pe.SmallMolecule(
         id=species.getId(),
         name=species.getName(),
@@ -159,9 +202,18 @@ def _parse_small_molecule(species: sbml.Species):
 
 
 def _parse_protein(species: sbml.Species):
-    """Parse a species from an SBML model into an EnzymeML small molecule."""
+    """
+    Parse a species from an SBML model into an EnzymeML protein.
 
-    annots = _parse_enzymeml_annotations("protein", species.getAnnotationString())
+    Args:
+        species (sbml.Species): The SBML species.
+
+    Returns:
+        An EnzymeML protein.
+    """
+    parsed = version.parse_annotation(annotation=species.getAnnotationString())
+    annots = version.extract(parsed, "protein")
+
     protein = pe.Protein(
         id=species.getId(),
         name=species.getName(),
@@ -176,9 +228,18 @@ def _parse_protein(species: sbml.Species):
 
 
 def _parse_complex(species: sbml.Species):
-    """Parse a species from an SBML model into an EnzymeML complex."""
+    """
+    Parse a species from an SBML model into an EnzymeML complex.
 
-    annots = _parse_enzymeml_annotations("complex", species.getAnnotationString())
+    Args:
+        species (sbml.Species): The SBML species.
+
+    Returns:
+        An EnzymeML complex.
+    """
+    parsed = version.parse_annotation(annotation=species.getAnnotationString())
+    annots = version.extract(parsed, "complex")
+
     complex_ = pe.Complex(
         id=species.getId(),
         name=species.getName(),
@@ -191,8 +252,15 @@ def _parse_complex(species: sbml.Species):
 
 
 def _parse_vessel(compartment: sbml.Compartment):
-    """Parse a compartment from an SBML model into an EnzymeML vessel."""
+    """
+    Parse a compartment from an SBML model into an EnzymeML vessel.
 
+    Args:
+        compartment (sbml.Compartment): The SBML compartment.
+
+    Returns:
+        An EnzymeML vessel.
+    """
     vessel = pe.Vessel(
         id=compartment.getId(),
         name=compartment.getName(),
@@ -206,8 +274,18 @@ def _parse_vessel(compartment: sbml.Compartment):
 
 
 def _parse_parameter(parameter: sbml.Parameter):
-    """Parse a parameter from an SBML model into an EnzymeML parameter."""
-    annots = _parse_enzymeml_annotations("parameter", parameter.getAnnotationString())
+    """
+    Parse a parameter from an SBML model into an EnzymeML parameter.
+
+    Args:
+        parameter (sbml.Parameter): The SBML parameter.
+
+    Returns:
+        An EnzymeML parameter.
+    """
+    parsed = version.parse_annotation(annotation=parameter.getAnnotationString())
+    annots = version.extract(parsed, "parameter")
+
     parameter = pe.Parameter(
         id=parameter.getId(),
         name=parameter.getName(),
@@ -222,8 +300,19 @@ def _parse_parameter(parameter: sbml.Parameter):
 
 
 def _parse_equation(rule: sbml.Rule, rule_type: pe.EquationType):
-    """Parse a rule from an SBML model into an EnzymeML equation."""
+    """
+    Parse a rule from an SBML model into an EnzymeML equation.
 
+    Args:
+        rule (sbml.Rule): The SBML rule.
+        rule_type (pe.EquationType): The type of the equation.
+
+    Returns:
+        An EnzymeML equation.
+
+    Raises:
+        ValueError: If the rule type is unknown.
+    """
     match rule_type:
         case pe.EquationType.INITIAL_ASSIGNMENT:
             equation = sbml.formulaToString(rule.getMath())
@@ -232,12 +321,17 @@ def _parse_equation(rule: sbml.Rule, rule_type: pe.EquationType):
             equation = rule.getFormula()
             species_id = rule.getVariable()
         case pe.EquationType.RATE_LAW:
-            equation = rule.getMath()
+            equation = rule.getFormula()
             species_id = None
         case _:
             raise ValueError(f"Unknown rule type: {rule_type}")
 
-    annots = _parse_enzymeml_annotations("variables", rule.getAnnotationString())
+    parsed = version.parse_annotation(annotation=rule.getAnnotationString())
+    annots = version.extract(parsed, "variables")
+
+    if version.version == SupportedVersions.VERSION1:
+        _map_v1_variables(annots, equation)
+
     equation = pe.Equation(
         equation_type=rule_type,
         equation=equation,
@@ -248,9 +342,43 @@ def _parse_equation(rule: sbml.Rule, rule_type: pe.EquationType):
     return equation
 
 
-def _parse_reaction(reaction: sbml.Reaction):
-    """Parse a reaction from an SBML model into an EnzymeML reaction."""
+def _map_v1_variables(annots: dict, equation: str):
+    """
+    Map variables for version 1 of the EnzymeML format.
 
+    Args:
+        annots (dict): The annotations.
+        equation (str): The equation string.
+    """
+    if "variables" not in annots:
+        annots["variables"] = []
+
+    all_species = [
+        *[s.id for s in enzmldoc.small_molecules],
+        *[p.id for p in enzmldoc.proteins],
+        *[c.id for c in enzmldoc.complexes],
+    ]
+    for species in all_species:
+        if bool(re.search(rf"\b{species}\b", equation)):
+            annots["variables"].append(
+                pe.Variable(
+                    id=species,
+                    name=species,
+                    symbol=species,
+                ).model_dump()
+            )
+
+
+def _parse_reaction(reaction: sbml.Reaction):
+    """
+    Parse a reaction from an SBML model into an EnzymeML reaction.
+
+    Args:
+        reaction (sbml.Reaction): The SBML reaction.
+
+    Returns:
+        An EnzymeML reaction.
+    """
     # Get the reactants and products
     products = [_parse_element(product, 1) for product in reaction.getListOfProducts()]
     reactants = [
@@ -261,7 +389,7 @@ def _parse_reaction(reaction: sbml.Reaction):
     modifiers = [modifier.getSpecies() for modifier in reaction.getListOfModifiers()]
 
     # Get the kinetic law
-    kinetic_law = _parse_kinetic_law(reaction)
+    kinetic_law = _parse_equation(reaction.getKineticLaw(), pe.EquationType.RATE_LAW)
 
     # Create the EnzymeML reaction
     reaction = pe.Reaction(
@@ -272,242 +400,68 @@ def _parse_reaction(reaction: sbml.Reaction):
         kinetic_law=kinetic_law,
     )
 
-    rich.print(reaction)
-
     return reaction
 
 
 def _parse_element(species, direction: int):
+    """
+    Parse a species element from an SBML model into an EnzymeML reaction element.
+
+    Args:
+        species: The SBML species element.
+        direction (int): The direction of the reaction (1 for product, -1 for reactant).
+
+    Returns:
+        An EnzymeML reaction element.
+    """
     return pe.ReactionElement(
         species_id=species.getSpecies(),
         stoichiometry=species.getStoichiometry() * direction,
     )
 
 
-def _parse_kinetic_law(reaction):
-    kinetic_law = reaction.getKineticLaw()
+def _parse_measurements(
+    model: sbml.Model,
+    list_of_reactions: sbml.ListOfReactions,
+    meas_data: dict[str, pd.DataFrame],
+):
+    """
+    Parse measurements from an SBML model into EnzymeML measurements.
 
-    if not kinetic_law:
-        return None
+    Args:
+        model (sbml.Model): The SBML model.
+        list_of_reactions (sbml.ListOfReactions): The list of reactions in the SBML model.
+        meas_data (dict[str, pd.DataFrame]): The measurement data.
 
-    annots = _parse_enzymeml_annotations("variables", kinetic_law.getAnnotationString())
-    kinetic_law = pe.Equation(
-        equation=kinetic_law.getFormula(),
-        equation_type=pe.EquationType.RATE_LAW,
-        unit=M,  # We need to set a default unit here, because SBML does not provide units for kinetic laws
-        variables=[pe.Variable(**annot) for annot in annots.get("variables", [])],
-    )
+    Returns:
+        EnzymeML measurements.
 
-    return kinetic_law
-
-
-def _parse_measurements(model: sbml.Model):
-    # Extract metadata from the model
-    meas_metadata, file_path = _get_meas_metadata(model)
-    meas_metadata = _extract_measurement_metadata(meas_metadata)
-
-    # Read the CSV file into measurement objects
-    # We need to set defaults for the units here,
-    # but we will overwrite them with the units from the EnzymeML annotations
-    measurements = pe.read_csv(
-        tsv_path,
-        data_unit=M,
-        time_unit=s,
-    )
-
-    for meas_id, metadata in meas_metadata.items():
-        _assign_units_and_initials(meas_id, measurements, metadata)
-
-    return measurements
-
-
-def _assign_units_and_initials(meas_id, measurements, metadata):
-    measurement = [m for m in measurements if m.id == meas_id][0]
-    conditions = metadata.get("condition", {})
-    init_concs = metadata.get("speciesData", [])
-    measurement.ph = _cast(conditions.get("ph"), float)
-    measurement.temperature = _cast(conditions.get("temperature"), float)
-    measurement.temperature_unit = conditions.get("temperature_unit")
-
-    for init_conc in init_concs:
-        species_id = _get_species_id(init_conc)
-        meas_data = _get_species_data(measurement, species_id)
-
-        meas_data.initial = float(init_conc.get("initial", 0.0))
-        meas_data.time_unit = init_conc["time_unit"]
-        meas_data.data_unit = init_conc["data_unit"]
-
-
-def _cast(value, data_type):
-    if value is None:
-        return None
-
-    return data_type(value)
-
-
-def _get_species_id(init_conc):
-    species_id = init_conc.get("species_id")
-    assert species_id, "No species ID found in the EnzymeML data annotation"
-    return species_id
-
-
-def _get_species_data(measurement, species_id):
-    try:
-        return [d for d in measurement.species_data if d.species_id == species_id][0]
-    except IndexError:
-        raise ValueError(f"No species data found for species ID {species_id}")
-
-
-def _get_meas_metadata(model):
-    root = ET.fromstring(model.getAnnotationString())
-    data = root.find(f"{{{ENZYMEML_NS}}}data")
-    meas_metadata = data.findall(f"{{{ENZYMEML_NS}}}measurement")
-    file_path = data.attrib.get("file")
-
-    assert file_path, "No file path found in the EnzymeML data annotation"
-
-    return meas_metadata, file_path
-
-
-def _extract_measurement_metadata(meas_metadata):
-    measurements = {}
-
-    for meas_meta in meas_metadata:
-        meas_id = meas_meta.attrib.get("id")
-        data = _process_measurement(meas_meta)
-        measurements[meas_id] = data
-
-    return measurements
-
-
-def _process_measurement(meas_meta):
-    data = {}
-    time_unit = units.get(meas_meta.attrib.get("timeUnit"))
-
-    assert (
-        time_unit
-    ), f"Time unit not found for measurement {meas_meta.attrib.get('id')}"
-
-    species_data = _parse_enzymeml_annotations("speciesData", meas_meta).get(
-        "speciesData", []
-    )
-    conditions = _parse_enzymeml_annotations("conditions", meas_meta).get(
-        "conditions", []
-    )
-
-    if conditions:
-        data["condition"] = _assign_temp_unit(conditions[0])
-    if species_data:
-        data["speciesData"] = _assign_meas_units(species_data, time_unit)
-
-    return data
-
-
-def _assign_temp_unit(conditions):
-    if temp_unit := conditions.get("temperature_unit"):
-        conditions["temperature_unit"] = units.get(temp_unit)
-
-    return conditions
-
-
-def _assign_meas_units(species_data, time_unit):
-    for data in species_data:
-        data["data_unit"] = units.get(data["data_unit"])
-        data["time_unit"] = time_unit
-        data["data_type"] = pe.DataTypes.__members__.get(data["data_type"])
-
-    return species_data
-
-
-def _parse_enzymeml_annotations(
-    root_key: str, annotation: str | ET.Element | None
-) -> dict:
-    """Parse the RDF annotation of an SBML object."""
-
-    if not annotation:
-        return {}
-
-    mappings = MAPPINGS[root_key]
-
-    match annotation:
-        case str():
-            root = ET.fromstring(annotation).find(f".//{{{ENZYMEML_NS}}}{root_key}")
-        case _ if isinstance(annotation, ET.Element):
-            root = annotation
+    Raises:
+        ValueError: If the version is unknown.
+    """
+    match version.version:
+        case SupportedVersions.VERSION1:
+            parsed = version.parse_annotation(list_of_reactions.getAnnotationString())
+            if parsed.data:
+                return parsed.data.to_measurements(meas_data, units)
+        case SupportedVersions.VERSION2:
+            parsed = version.parse_annotation(model.getAnnotationString())
+            if parsed.data:
+                return parsed.data.to_measurements(meas_data, units)
         case _:
-            raise ValueError(f"Unknown annotation type: {type(annotation)}")
-
-    if not root:
-        return {}
-
-    return _map_values(mappings, root, root_key)
-
-
-def _map_values(mappings, root, root_key=None):
-    """Map the values from the XML root to the EnzymeML entities."""
-
-    data = {}
-    for xml_key, enzml_key in mappings.items():
-        match enzml_key:
-            case dict():
-                if value := _process_nested_keys(
-                    enzml_key=enzml_key,
-                    root=root,
-                    xml_key=xml_key,
-                ):
-                    if root_key in data and len(value) == 1:
-                        # Special case for conditions
-                        data[root_key][0].update(value[0])
-                    else:
-                        data[root_key] = value
-
-            case str():
-                if value := _process_simple(
-                    root=root,
-                    xml_key=xml_key,
-                ):
-                    data[enzml_key] = value
-            case _:
-                raise ValueError(f"Unknown value type for key '{enzml_key}'")
-
-    return data
-
-
-def _process_simple(
-    root: ET.Element,
-    xml_key: str,
-):
-    if xml_key.startswith("@"):
-        value = root.attrib.get(xml_key[1:])
-    else:
-        value = root.findall(f"{{{ENZYMEML_NS}}}{xml_key}")
-
-        if len(value) == 1:
-            value = value[0].text
-        elif len(value) > 1:
-            value = [v.text for v in value]
-    return value
-
-
-def _process_nested_keys(
-    enzml_key: dict,
-    root: ET.Element,
-    xml_key: str,
-):
-    value = root.findall(f".//{{{ENZYMEML_NS}}}{xml_key}")
-    if len(value) == 1:
-        value = [_map_values(enzml_key, value[0])]
-    elif len(value) > 1:
-        value = [_map_values(enzml_key, v) for v in value]
-    else:
-        return None
-
-    return value
+            raise ValueError(f"Unknown version: {version.version}")
 
 
 def _check_nan(value: float):
-    """Check if a value is NaN."""
+    """
+    Check if a value is NaN.
 
+    Args:
+        value (float): The value to check.
+
+    Returns:
+        The value if it is not NaN, otherwise None.
+    """
     if math.isnan(value):
         return None
 
