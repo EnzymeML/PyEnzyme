a2c3d3566ae613cfd94bbad699a61160911aa97c
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-05-07 01:19:17 +0200

    remove dev examples

--- dev-examples/sbml_import_legacy.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-import pyenzyme as pe
-
-doc = pe.EnzymeMLDocument.from_sbml(path=Path("./tests/fixtures/sbml/v1_example.omex"))
-
-with open("./tests/fixtures/sbml/v1_example_enzml.json", "w") as f:
-    f.write(doc.model_dump_json(indent=2))
