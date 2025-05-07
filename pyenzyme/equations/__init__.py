2fafd36ade122347c59192f9f1ca5cfddf5e4897
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-05-07 12:25:12 +0200

    rename modules

--- pyenzyme/equations/__init__.py
+++ pyenzyme/equations/__init__.py
@@ -1,2 +1,4 @@
-from ._math import build_equation, build_equations  # noqa: F401
-from ._chem import build_reaction, build_reactions  # noqa: F401
+from .math import build_equation, build_equations  # noqa: F401
+from .chem import build_reaction, build_reactions  # noqa: F401
+
+__all__ = ["build_equation", "build_equations", "build_reaction", "build_reactions"]
