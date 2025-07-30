f1fa11c2f368ac0ef8a3a0df1affc23b98849ed9
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-07-30 11:39:53 +0200

    Remove unused read_static_file function
    Deleted the read_static_file function from tools.py as it is no longer used in the codebase.

--- pyenzyme/tools.py
+++ pyenzyme/tools.py
@@ -70,22 +70,6 @@ def get_all_parameters(enzmldoc):
     return find_unique(enzmldoc, target=pe.Parameter)
 
 
-def read_static_file(path, filename: str):
-    """Reads a static file from the specified library path.
-
-    Args:
-        path (Module): Import path of the library.
-        filename (str): The name of the file to read.
-
-    Returns:
-        dict: The contents of the file as a dictionary.
-    """
-
-    source = pkg_resources.files(path).joinpath(filename)
-    with pkg_resources.as_file(source) as file:
-        return toml.load(file)
-
-
 def find_unique(obj, target):
     """Composite function that extracts all instances of a specified target type from a given object.
 
